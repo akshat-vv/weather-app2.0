629a557d1cf72a84267fc07821e8b65da2687d4b
akshat-vv <akshatvijay14@gmail.com>
2023-11-14 13:09:29 +0530

    Updates

---
+++ precache-manifest.4ced0b5fbe5d296c13231b41627216fb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600ae7c942094f6cf59b3867ed4a98d0",
+    "url": "/weather-app2.0/index.html"
+  },
+  {
+    "revision": "1f30e24c1678491b3f6d",
+    "url": "/weather-app2.0/static/css/main.edc1dc17.chunk.css"
+  },
+  {
+    "revision": "001b67d05a335e326bff",
+    "url": "/weather-app2.0/static/js/2.6a7fe4e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app2.0/static/js/2.6a7fe4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f30e24c1678491b3f6d",
+    "url": "/weather-app2.0/static/js/main.b25db939.chunk.js"
+  },
+  {
+    "revision": "a75d9b437e7cdf29ea91",
+    "url": "/weather-app2.0/static/js/runtime-main.82447ce9.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/weather-app2.0/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "4d4c95f24bccfba7c1859772f23cf9fd",
+    "url": "/weather-app2.0/static/media/background.4d4c95f2.jpg"
+  },
+  {
+    "revision": "a00ef1802deb1facdb552d03150a0008",
+    "url": "/weather-app2.0/static/media/city.a00ef180.jpg"
+  }
+]);
